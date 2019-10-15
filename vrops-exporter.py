fe7817c0b4f9b37e6a7ce66269a5c26d3949da59
d064689 <christopher.hans@sap.com>
2019-10-15 10:31:31 +0200

    added get method and config read

--- vrops-exporter.py
+++ vrops-exporter.py
@@ -1,39 +1,69 @@
 import requests
-from prometheus_client import Metric, start_http_server, REGISTRY
+from requests.auth import HTTPBasicAuth
+import sys
+from prometheus_client import Metric, start_http_server, CollectorRegistry
+from prometheus_client.exposition import MetricsHandler, choose_encoder
 import json
 import time
+from urllib.parse import urlparse, parse_qs, parse_qsl
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
+from module.read_yaml import YamlRead
+
+
+def do_GET(self):
+    registry = CollectorRegistry()
+    params = parse_qs(urlparse(self.path).query)
+    print(params)
+    if len(params.keys()) != 0:
+        try:
+            print(params['target'])
+            collector = Collector(params['target'][0], config)
+        except Exception as e:
+            print("obviously missing params: " + json.dumps(params))
+            print(e)
+            return
+        try:
+            registry.register(collector)
+        except Exception as e:
+            print("failed to register: " + json.dumps(params))
+            print(e)
+            return
+
+    encoder, content_type = choose_encoder(self.headers.get('Accept'))
+    try:
+        output = encoder(registry)
+    except:
+        self.send_error(500, 'error generating metric output')
+        raise
+    self.send_response(200)
+    self.send_header('Content-Type', content_type)
+    self.end_headers()
+    self.wfile.write(output)
+
+
+MetricsHandler.do_GET = do_GET
+
 
 class Collector(object):
 
-    def __init__(self, target, user, password):
+    def __init__(self, target, config):
         self._target = target
-        self._user = user
-        self._password = password
+        self._user = config['user']
+        self._password = config['password']
 
     def collect(self):
-        url = "https://vcops01.wdf.sap.corp/suite-api/api/resources"
 
+        url = "https://" + self._target + "/suite-api/api/resources"
         querystring = {"resourceKind": "host"}
-
-        payload = "{\n  \"username\" : \"prometheusapiuser\",\n  \"password\" : \"2SNNg/&GCsyN\"\n}\n"
         headers = {
             'Content-Type': "application/json",
-            'Authorization': "Basic cHJvbWV0aGV1c2FwaXVzZXI6MlNOTmcvJkdDc3lO",
-            'User-Agent': "PostmanRuntime/7.17.1",
-            'Accept': "application/json",
-            'Cache-Control': "no-cache",
-            'Postman-Token': "b1eed2c7-4bc3-46ad-aac8-19a7dbd9fdfd,a9ad9a2a-3d1b-4063-b9aa-70184deb04ae",
-            'Host': "vcops01.wdf.sap.corp",
-            'Accept-Encoding': "gzip, deflate",
-            'Content-Length': "70",
-            'Connection': "keep-alive",
-            'cache-control': "no-cache"
+            'Accept': "application/json"
         }
 
-        response = requests.request("GET", url, data=payload, headers=headers, params=querystring, verify=False)
-        response.
+        response = requests.get(url, auth=HTTPBasicAuth(self._user, self._password), verify=False, params=querystring,
+                                headers=headers)
 
+        print(response)
         json_response = response.json()
         for resource in json_response["resourceList"]:
             print(resource["identifier"], resource["resourceKey"]["name"])
@@ -52,13 +82,13 @@ class Collector(object):
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-# main-Klasse
+# main
 if __name__ == '__main__':
-    # Start the Prometheus http server and register exporter class.
-    start_http_server(1234)
+    # Read yaml file
+    config = YamlRead(sys.argv[1]).run()
 
-    # vrops registrieren
-    registry = REGISTRY.register(Collector(target="vcops01", user="prometheusapiuser", password="2SNNg/&GCsyN"))
+    # Start the Prometheus http server.
+    start_http_server(config['port'])
 
     while True:
         time.sleep(1)
