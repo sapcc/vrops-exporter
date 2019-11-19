5a6af49f7e309189ecce1ca3f5e7b0d9356c9311
Tommy Sauer <tommy.sauer@sap.com>
2019-11-19 16:36:36 +0100

    deprecated configfile introduced cli options introduced env options

--- vrops-exporter.py
+++ vrops-exporter.py
@@ -4,22 +4,38 @@ import sys
 import time
 import os
 from prometheus_client import start_http_server
-from tools.YamlRead import YamlRead
 import VropsCollector
+from optparse import OptionParser
 
 if __name__ == '__main__':
-    # Read yaml file
-    config = YamlRead(sys.argv[1]).run()
-    os.environ['USER'] = config['user']
-    os.environ['PASSWORD'] = config['password']
+    parser = OptionParser()
+    parser.add_option("-u", "--user", help="specify user to log in", action="store", dest="user")
+    parser.add_option("-p", "--password", help="specify password to log in", action="store", dest="password")
+    parser.add_option("-o", "--port", help="specify exporter port", action="store", dest="port")
+    parser.add_option("-d", "--debug", help="enable debug", action="store_true", dest="debug", default=False)
+    (options, args) = parser.parse_args()
 
-    # Debug option
-    os.environ['DEBUG'] = '0'
-    if config['debug'] is True:
-        os.environ['DEBUG'] = '1'
+    if options.user:
+        os.environ['USER'] = options.user
+    if options.password:
+        os.environ['PASSWORD'] = options.password
+    if options.debug:
+        os.environ['DEBUG'] = True
+    if options.port:
+        os.environ['PORT'] = options.port
+
+    if "PORT" not in os.environ and not options.port:
+        print("Can't start, please specify port with ENV or -o")
+        sys.exit(0)
+    if "USER" not in os.environ and not options.user:
+        print("Can't start, please specify user with ENV or -u")
+        sys.exit(0)
+    if "PASSWORD" not in os.environ and not options.password:
+        print("Can't start, please specify password with ENV or -p")
+        sys.exit(0)
 
     # Start the Prometheus http server.
-    start_http_server(config['port'])
+    start_http_server(int(os.environ['PORT']))
 
     while True:
         time.sleep(1)
