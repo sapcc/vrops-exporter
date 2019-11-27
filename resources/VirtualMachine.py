abcdc21413caab2fa5e3d837b6b03d85dd127dbf
I520251 <richard.tief.sap.de>
2019-11-27 08:26:45 +0100

    update worktree

--- resources/VirtualMachine.py
+++
@@ -1,7 +0,0 @@
-class VirtualMachine:
-
-    def __init__(self, name, uuid, project_id):
-        self.name = name
-        self.uuid = uuid
-        self.project_id = project_id
-
--- resources/__init__.py
+++
@@ -1,5 +0,0 @@
-from resources.Vcenter import Vcenter
-from resources.Datacenter import Datacenter
-from resources.Cluster import Cluster
-from resources.Host import Host
-from resources.VirtualMachine import VirtualMachine
