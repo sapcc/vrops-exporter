7192ca0c270d9d47ea3f76bbec9b797ef8f1c8e6
richardtief <mail@richardtief.de>
2020-01-17 09:44:11 +0100

    HostSystemCollector is working properly

--- module/HostSystemCollector.py
+++ module/HostSystemCollector.py
@@ -1,6 +1,7 @@
+import os
 from BaseCollector import BaseCollector
 from prometheus_client.core import GaugeMetricFamily
-from tools.get_metrics import get_metrics
+from tools.get_metrics import get_metric
 
 
 class HostSystemCollector(BaseCollector):
@@ -10,50 +11,50 @@ class HostSystemCollector(BaseCollector):
         self._target = target
         self._user = user
         self._password = password
+        self._statkeys = [{"name": "Hardware|CPU Information|Number of CPUs (Cores)",
+                         "statkey": "hardware|cpuinfo|numCpuCores)"},
+                        {"name": "CPU|Number of CPU Sockets",
+                         "statkey": "cpu|numpackages"},
+                        {"name": "Memory|Swap Used (KB)",
+                         "statkey": "mem|swapused_average"},
+                        {"name": "Memory|Usable Memory (KB)",
+                         "statkey": "mem|host_usable"},
+                        {"name": "Memory|Usage (%)",
+                         "statkey": "mem|usage_average"},
+                        {"name": "Memory|Balloon (KB)",
+                         "statkey": "mem|vmmemctl_average"},
+                        {"name": "Memory|Compressed (KB)",
+                         "statkey": "mem|compressed_average"},
+                        {"name": "Memory|Guest Active (KB)",
+                         "statkey": "mem|active_average"},
+                        {"name": "Memory|Consumed (KB)",
+                         "statkey": "mem|consumed_average"},
+                        {"name": "Memory|Capacity Available to VMs (KB)",
+                         "statkey": "mem|totalCapacity_average"},
+                        {"name": "Memory|Total Capacity (KB)",
+                         "statkey": "mem|host_provisioned"},
+                        {"name": "Summary|Number of Running VMs",
+                         "statkey": "summary|number_running_vms"},
+                        {"name": "Network|Packets Dropped",
+                         "statkey": "net|droppedRx_summation"},
+                        {"name": "Network|Transmitted Packets Dropped",
+                         "statkey": "net|droppedTx_summation"},
+                        {"name": "Network|Packets Dropped (%)",
+                         "statkey": "net|droppedPct"}]
 
     def collect(self):
-
-        statkeys = [{"name": "Hardware|CPU Information|Number of CPUs (Cores)",
-                     "statkey": "hardware|cpuinfo|numCpuCores)"},
-                    {"name": "CPU|Number of CPU Sockets",
-                     "statkey": "cpu|numpackages"},
-                    {"name": "Memory|Swap Used (KB)",
-                     "statkey": "mem|swapused_average"},
-                    {"name": "Memory|Usable Memory (KB)",
-                     "statkey": "mem|host_usable"},
-                    {"name": "Memory|Usage (%)",
-                     "statkey": "mem|usage_average"},
-                    {"name": "Memory|Balloon (KB)",
-                     "statkey": "mem|vmmemctl_average"},
-                    {"name": "Memory|Compressed (KB)",
-                     "statkey": "mem|compressed_average"},
-                    {"name": "Memory|Guest Active (KB)",
-                     "statkey": "mem|active_average"},
-                    {"name": "Memory|Consumed (KB)",
-                     "statkey": "mem|consumed_average"},
-                    {"name": "Memory|Capacity Available to VMs (KB)",
-                     "statkey": "mem|totalCapacity_average"},
-                    {"name": "Memory|Total Capacity (KB)",
-                     "statkey": "mem|host_provisioned"},
-                    {"name": "Summary|Number of Running VMs",
-                     "statkey": "summary|number_running_vms"},
-                    {"name": "Network|Packets Dropped",
-                     "statkey": "net|droppedRx_summation"},
-                    {"name": "Network|Transmitted Packets Dropped",
-                     "statkey": "net|droppedTx_summation"},
-                    {"name": "Network|Packets Dropped (%)",
-                     "statkey": "net|droppedPct"},]
         metric_list = []
-
-        g = GaugeMetricFamily('vrops_resource_gauge', 'HostSystemCollector',
-                              labels=['target', 'name', 'uuid'])
-
+        g = GaugeMetricFamily('HostSystemCollector', self._target,
+                              labels=['name', 'statkey'])
         for cluster in self._resources.datacenter[0].clusters:
             for host in cluster.hosts:
-                for statkey in statkeys:
-                    get_metrics(self._target, host.name, host.uuid, statkey["statkey"])
-                    g.add_metric(labels=[self._target, host.name, host.uuid,  ], value=)
-
-        metric_list.append(g)
-
+                for statkey in self._statkeys:
+                    if os.environ['DEBUG'] == '1':
+                        print(host.name, "--add statkey:", statkey["name"])
+                    value = get_metric(self._target, host.uuid, statkey["statkey"])
+                    if value is not None:
+                        g.add_metric(labels=[host.name, statkey["name"]],
+                                     value=value)
+                        metric_list.append(g)
         return metric_list
+
--- tools/get_metrics.py
+++ tools/get_metrics.py
@@ -5,31 +5,24 @@ from urllib3.exceptions import HTTPError
 from requests.auth import HTTPBasicAuth
 
 
-def get_metrics(target, name, uuid, key):
-    outdata = []
-    resourcedata = {}
-    resourcedata["name"] = name
-
+def get_metric(target, uuid, key):
     url = "https://" + target + "/suite-api/api/resources/" + uuid + "/stats/latest"
-
     headers = {
         'Content-Type': "application/json",
         'Accept': "application/json"
     }
     disable_warnings(exceptions.InsecureRequestWarning)
-
     try:
         response = requests.get(url,
                                 auth=HTTPBasicAuth(username=os.environ['USER'], password=os.environ['PASSWORD']),
                                 verify=False,
                                 headers=headers)
         try:
-            if response.json()["values"][0]["stat-list"]["stat"]["statKey"]["key"] == key:
-                resourcedata["value"] = response.json()["values"][0]["stat-list"]["stat"]["data"]
-            outdata.append(resourcedata)
+            for statkey in response.json()["values"][0]["stat-list"]["stat"]:
+                if statkey["statKey"]["key"] is not None and statkey["statKey"]["key"] == key:
+                    return statkey["data"][0]
         except AttributeError as ar:
             print("There is no attribute: stat", ar.args)
     except HTTPError as err:
         print("Request failed: ", err.args)
 
-    return outdata
