c71dcd0dfb575a462665f1480001d098c4b7a43e
Sunny Arora <root@garuda-cc.openstack.na-us-1.cloud.sap>
2020-03-13 18:49:10 +0000

    modified exporter.py

--- exporter.py
+++ exporter.py
@@ -16,6 +16,7 @@ from collectors.HostSystemStatsCollector import HostSystemStatsCollector
 from collectors.HostSystemPropertiesCollector import HostSystemPropertiesCollector
 from collectors.DatastoreStatsCollector import DatastoreStatsCollector
 from collectors.VMstatCollector import VMstatCollector
+from collectors.VMPropertyCollector import VMPropertyCollector
 
 def parse_params():
     parser = OptionParser()
