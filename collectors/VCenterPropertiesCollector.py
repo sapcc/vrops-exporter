a4f7b9b5daaadbf09dad6ca1535e9d9205cc73ac
“i820959“ <carl.durr@sap.com>
2020-03-17 10:36:25 -0400

    improved prometheus labels

--- collectors/VCenterPropertiesCollector.py
+++ collectors/VCenterPropertiesCollector.py
@@ -1,9 +1,8 @@
 from BaseCollector import BaseCollector
-import os, time, json
+import os, time
 from prometheus_client.core import GaugeMetricFamily
 from tools.Resources import Resources
 from tools.YamlRead import YamlRead
-from pprint import pprint
 
 
 class VcenterPropertiesCollector(BaseCollector):
@@ -21,34 +20,34 @@ class VcenterPropertiesCollector(BaseCollector):
             print('vCenterPropertiesCollector starts with collecting the properties')
 
         g = GaugeMetricFamily('vrops_vcenter_properties', 'testtest',
-                              labels=['vcenter', 'propkey'])
+                              labels=['datacenter', 'vcenter', 'propkey'])
 
         for vc in self.get_vcenters():
-
             target = self.vcenters[vc]['target']
+            target_vc = self.vcenters[vc]['name']
             token = self.vcenters[vc]['token']
             vc_uuid = self.vcenters[vc]['uuid']
+            for cl in self.get_clusters():
+                if self.clusters[cl]['vcenter'] == target_vc:
+                    dc_name = self.clusters[cl]['parent_dc_name']
 
             for property_pair in self.property_yaml["VCenterPropertiesCollector"]['info_metrics']:
                 property_label = property_pair['label']
                 propkey = property_pair['property']
 
                 vc_values = Resources.get_property(target, token, vc_uuid, propkey)
-                print(vc_values)
-                g.add_metric(labels=[self.vcenters[vc]['name'], property_label], value="{}".format(vc_values))
 
                 if not vc_values:
                     continue
-                for value_entry in vc_values:
-                    print(value_entry)
-                    try:
-                        info_value = int(value_entry)
-                        g.add_metric(labels=[self.vcenters[vc]['name'], property_label], value=info_value)
-                    except (ValueError, TypeError):
-                        info = vc_values
-                        info_value = 0
-                        g.add_metric(labels=[self.vcenters[vc]['name'],
-                                     property_label + ": " + info], value=info_value)
+                try:
+                    info_value = int(vc_values)
+                    g.add_metric(labels=[dc_name, self.vcenters[vc]['name'],
+                                         property_label], value=info_value)
+                except (ValueError, TypeError):
+                    info = vc_values
+                    info_value = 0
+                    g.add_metric(labels=[dc_name, self.vcenters[vc]['name'],
+                                         property_label + ":" + info], value=info_value)
 
 
             yield g
