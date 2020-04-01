117090057d59e0c22c7344d1b82235ad5136ebef
Tommy Sauer <tommy.sauer@sap.com>
2020-04-01 15:24:58 +0200

    reenable vm collection again

--- InventoryBuilder.py
+++ InventoryBuilder.py
@@ -172,10 +172,10 @@ class InventoryBuilder:
                         for ds_object in hs_object.datastores:
                             if os.environ['DEBUG'] >= '2':
                                 print("Collecting Datastore: " + ds_object.name)
-                        # hs_object.add_vm()
-                        # for vm_object in hs_object.vms:
-                            # if os.environ['DEBUG'] >= '2':
-                                # print("Collecting VM: " + vm_object.name)
+                        hs_object.add_vm()
+                        for vm_object in hs_object.vms:
+                            if os.environ['DEBUG'] >= '2':
+                                print("Collecting VM: " + vm_object.name)
             return vcenter
 
     def get_vcenters(self):
