b356a12ee214ce9c6509c8213e09ac48f514046d
durcar86 <durcar86@gmail.com>
2020-04-07 14:54:01 -0400

    Update VCenterPropertiesCollector.py
    new code thanks to Sunny

--- collectors/VCenterPropertiesCollector.py
+++ collectors/VCenterPropertiesCollector.py
@@ -1,26 +1,23 @@
 from BaseCollector import BaseCollector
-import os, time
+import os, time, json
 from prometheus_client.core import GaugeMetricFamily
 from tools.Resources import Resources
 from tools.YamlRead import YamlRead
 
-
 class VcenterPropertiesCollector(BaseCollector):
     def __init__(self):
-        self.iteration = 0
-        while not self.iteration:
-            time.sleep(5)
-            self.get_iteration()
-            print("waiting for initial iteration")
-        print("done: initial query")
+        self.wait_for_inventory_data()
         self.property_yaml = YamlRead('collectors/property.yaml').run()
+        self.g = GaugeMetricFamily('vrops_vcenter_properties', 'testtest',
+                              labels=['datacenter', 'vcenter', 'propkey'])
+        self.post_registered_collector(self.__class__.__name__, self.g.name)
+
+    def describe(self):
+        yield self.g
 
     def collect(self):
         if os.environ['DEBUG'] >= '1':
             print('vCenterPropertiesCollector starts with collecting the properties')
-
-        g = GaugeMetricFamily('vrops_vcenter_properties', 'testtest',
-                              labels=['datacenter', 'vcenter', 'propkey'])
 
         for vc in self.get_vcenters():
             target = self.vcenters[vc]['target']
@@ -30,6 +27,9 @@ class VcenterPropertiesCollector(BaseCollector):
             for cl in self.get_clusters():
                 if self.clusters[cl]['vcenter'] == target_vc:
                     dc_name = self.clusters[cl]['parent_dc_name']
+
+            if not token:
+                print("skipping " + target + " in vCenterPropertiesCollector, no token")
 
             for property_pair in self.property_yaml["VcenterPropertiesCollector"]['info_metrics']:
                 property_label = property_pair['label']
@@ -41,13 +41,13 @@ class VcenterPropertiesCollector(BaseCollector):
                     continue
                 try:
                     info_value = int(vc_values)
-                    g.add_metric(labels=[dc_name, target_vc,
+                    self.g.add_metric(labels=[dc_name, target_vc,
                                          property_label], value=info_value)
                 except (ValueError, TypeError):
                     info = vc_values
                     info_value = 0
-                    g.add_metric(labels=[dc_name, target_vc,
+                    self.g.add_metric(labels=[dc_name, target_vc,
                                          property_label + ": " + info], value=info_value)
 
-
-            yield g
+            self.post_metrics(self.g.name)
+            yield self.g
