07944e13e4d2ffef43c15c3207355df62aab8945
Richard Tief <richard.tief@sap.com>
2020-04-14 11:34:54 +0200

    return a empty list instead of bool

--- tools/Resources.py
+++ tools/Resources.py
@@ -88,7 +88,7 @@ class Resources:
                                     headers=headers)
         except Exception as e:
             print("Problem connecting to " + target + "Error: " + str(e))
-            return []
+            return resources
 
         if response.status_code == 200:
             for resource in response.json()["resourceList"]:
