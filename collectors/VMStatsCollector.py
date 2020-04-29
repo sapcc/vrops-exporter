e019d0257dd67babe141291c96edf5e7cebdbe4a
Tommy Sauer <tommy.sauer@sap.com>
2020-04-29 15:32:05 +0200

    adding vmstats

--- collectors/VMStatsCollector.py
+++ collectors/VMStatsCollector.py
@@ -3,6 +3,7 @@ import os
 from prometheus_client.core import GaugeMetricFamily
 from tools.Resources import Resources
 from tools.YamlRead import YamlRead
+from threading import Thread
 
 
 class VMStatsCollector(BaseCollector):
@@ -19,33 +20,45 @@ class VMStatsCollector(BaseCollector):
         if os.environ['DEBUG'] >= '1':
             print('VMStatsCollector starts with collecting the metrics')
 
-       # #make one big request per stat id with all resource id's in its belly
+        thread_list = list()
         for target in self.get_vms_by_target():
-            token = self.get_target_tokens()
-            token = token[target]
-            if not token:
-                print("skipping " + target + " in VMStatsCollector, no token")
+            print("threading for", target)
+            t = Thread(target=self.do_vm_metrics, args=(target,g))
+            thread_list.append(t)
+            t.start()
 
-            uuids = self.target_vms[target]
-            for statkey_pair in self.statkey_yaml["VMStatsCollector"]:
-                statkey_label = statkey_pair['label']
-                statkey = statkey_pair['statkey']
-                values = Resources.get_latest_stat_multiple(target, token, uuids, statkey)
-                if not values:
-                    print("skipping statkey " + str(statkey) + " in VMStatsCollector, no return")
-                    continue
-                for value_entry in values:
-                    if 'resourceId' not in value_entry:
-                        continue
-                    #there is just one, because we are querying latest only
-                    metric_value = value_entry['stat-list']['stat'][0]['data']
-                    if not metric_value:
-                        continue
-                    vm_id = value_entry['resourceId']
-                    if vm_id not in self.vms:
-                        continue
-                    g.add_metric(labels=[self.vms[vm_id]['cluster'], self.vms[vm_id]['datacenter'],
-                                self.vms[vm_id]['name'], self.vms[vm_id]['parent_host_name'], statkey_label],
-                                 value=metric_value[0])
+        for t in thread_list:
+            print("joining")
+            t.join()
+
+       # #make one big request per stat id with all resource id's in its belly
         # self.post_metrics(g.name)
         yield g
+
+    def do_vm_metrics(self, target, g):
+        token = self.get_target_tokens()
+        token = token[target]
+        if not token:
+            print("skipping " + target + " in VMStatsCollector, no token")
+
+        uuids = self.target_vms[target]
+        for statkey_pair in self.statkey_yaml["VMStatsCollector"]:
+            statkey_label = statkey_pair['label']
+            statkey = statkey_pair['statkey']
+            values = Resources.get_latest_stat_multiple(target, token, uuids, statkey)
+            if not values:
+                print("skipping statkey " + str(statkey) + " in VMStatsCollector, no return")
+                continue
+            for value_entry in values:
+                if 'resourceId' not in value_entry:
+                    continue
+                #there is just one, because we are querying latest only
+                metric_value = value_entry['stat-list']['stat'][0]['data']
+                if not metric_value:
+                    continue
+                vm_id = value_entry['resourceId']
+                if vm_id not in self.vms:
+                    continue
+                g.add_metric(labels=[self.vms[vm_id]['cluster'], self.vms[vm_id]['datacenter'],
+                                self.vms[vm_id]['name'], self.vms[vm_id]['parent_host_name'], statkey_label],
+                                 value=metric_value[0])
