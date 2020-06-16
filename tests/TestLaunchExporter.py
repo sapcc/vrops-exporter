cc2687dde495281e2cb074f9b580661bda60fdec
Richard Tief <richard.tief@sap.com>
2020-06-16 11:37:13 +0200

    changed path to config file

--- tests/TestLaunchExporter.py
+++ tests/TestLaunchExporter.py
@@ -12,37 +12,37 @@ class TestLaunchExporter(TestCase):
 
     # test with debug option on
     def test_with_cli_params_1(self):
-        sys.argv = ['prog', '-o', '1234', '-d', '-i', 'inventory.some.url', '-m', 'collector_config.yaml']
+        sys.argv = ['prog', '-o', '1234', '-d', '-i', 'inventory.some.url', '-m', 'tests/collector_config.yaml']
         parse_params()
         self.assertEqual(os.getenv('PORT'), '1234', 'The port was not set correctly!')
         self.assertEqual(os.getenv('DEBUG'), '1', 'Debug was not set correctly!')
         self.assertEqual(os.getenv('INVENTORY'), 'inventory.some.url', 'Inventory was not set correctly')
-        self.assertEqual(os.getenv('CONFIG'), 'collector_config.yaml', 'Config was not set')
+        self.assertEqual(os.getenv('CONFIG'), 'tests/collector_config.yaml', 'Config was not set')
 
     # test with debug option off
     def test_with_cli_params_2(self):
         os.environ.clear()
-        sys.argv = ['prog', '--port', '1234', '-i', 'inventory.some.url', '-m', 'collector_config.yaml']
+        sys.argv = ['prog', '--port', '1234', '-i', 'inventory.some.url', '-m', 'tests/collector_config.yaml']
         parse_params()
         self.assertEqual(os.getenv('PORT'), '1234', 'The port was not set correctly!')
         self.assertEqual(os.getenv('DEBUG'), '0', 'Debug was not set correctly')
         self.assertEqual(os.getenv('INVENTORY'), 'inventory.some.url', 'Inventory was not set correctly')
-        self.assertEqual(os.getenv('CONFIG'), 'collector_config.yaml', 'Config was not set')
+        self.assertEqual(os.getenv('CONFIG'), 'tests/collector_config.yaml', 'Config was not set')
 
     def test_with_cli_and_env_params(self):
-        sys.argv = ['prog', '--port', '1234', "--debug", '-i', 'inventory.some.url', '-m', 'collector_config.yaml']
+        sys.argv = ['prog', '--port', '1234', "--debug", '-i', 'inventory.some.url', '-m', 'tests/collector_config.yaml']
         os.environ['PORT'] = '1123'
         os.environ['INVENTORY'] = 'inventory.wedontwantthis.url'
-        os.environ['CONFIG'] = 'collector_config.yaml'
+        os.environ['CONFIG'] = 'tests/collector_config.yaml'
         parse_params()
         # cli params preferred
         self.assertEqual(os.getenv('PORT'), '1234', 'The port was not set correctly!')
         self.assertEqual(os.getenv('INVENTORY'), 'inventory.some.url', 'Inventory was not set correctly')
-        self.assertEqual(os.getenv('CONFIG'), 'collector_config.yaml', 'Config was not set')
+        self.assertEqual(os.getenv('CONFIG'), 'tests/collector_config.yaml', 'Config was not set')
 
     # test use default collectors when nothing is specified
     def test_with_no_collector(self):
-        sys.argv = ['prog', '--port', '1234', '-i', 'inventory.some.url', '-m', 'collector_config.yaml']
+        sys.argv = ['prog', '--port', '1234', '-i', 'inventory.some.url', '-m', 'tests/collector_config.yaml']
         options = parse_params()
         self.assertEqual(options.collectors, default_collectors(), 'Default collector list does not match the default')
 
@@ -57,7 +57,7 @@ class TestLaunchExporter(TestCase):
     # test multiple enabled collectors
     def test_with_multiple_collector(self):
         sys.argv = ['prog', '--port', '1234', '-i', 'inventory.some.url', '-c', 'VMStatsCollector', '-m',
-                    'collector_config.yaml',
+                    'tests/collector_config.yaml',
                     '-c', 'VMPropertiesCollector']
         options = parse_params()
         self.assertEqual(options.collectors, ['VMStatsCollector', 'VMPropertiesCollector'],
