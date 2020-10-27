a452dff43a90111c03fc34fd1dfcae9ffa0c4f6a
Richard Tief <richard.tief@sap.com>
2020-10-27 14:44:24 +0100

    sharding VMStatsCollector

--- collectors/VMStatsCollector.py
+++ collectors/VMStatsCollector.py
@@ -8,12 +8,14 @@ class VMStatsCollector(BaseCollector):
     def __init__(self):
         super().__init__()
         self.vrops_entity_name = 'virtualmachine'
+        self.rubric = os.environ['RUBRIC']
         self.wait_for_inventory_data()
         self.name = self.__class__.__name__
 
     def collect(self):
         gauges = self.generate_gauges('stats', self.name, self.vrops_entity_name,
-                                      [self.vrops_entity_name, 'vcenter', 'datacenter', 'vccluster', 'hostsystem', 'project'])
+                                      [self.vrops_entity_name, 'vcenter', 'datacenter', 'vccluster', 'hostsystem',
+                                       'project'], rubric=self.rubric)
         project_ids = self.get_project_ids_by_target()
 
         if os.environ['DEBUG'] >= '1':
--- tests/TestCollectors.py
+++ tests/TestCollectors.py
@@ -37,8 +37,8 @@ class TestCollectors(unittest.TestCase):
     def test_collector_metrics(self):
         metrics_yaml = yaml_read('tests/metrics.yaml')
         # every collector got to be tested in here
-        random_prometheus_port = random.randrange(9000, 9700, 1)
-        print("chosen testport: " + str(random_prometheus_port))
+        self.random_prometheus_port = random.randrange(9000, 9700, 1)
+        print("chosen testport: " + str(self.random_prometheus_port))
 
         Vrops.get_token = MagicMock(return_value="2ed214d523-235f-h283-4566-6sf356124fd62::f234234-234")
         Vrops.get_adapter = MagicMock(return_value=[{'name': "vcenter1", 'uuid': '5628-9ba1-55e84701'}])
@@ -75,20 +75,26 @@ class TestCollectors(unittest.TestCase):
         thread.daemon = True
         thread.start()
 
-        for collector in metrics_yaml.keys():
-            print("\nTesting " + collector)
-
+        def collector_testrun(rubric=None):
             if 'Stats' in collector:
                 # mocking all values from yaml
                 statkey_yaml = yaml_read(os.environ['CONFIG'])['statkeys']
                 multiple_metrics_generated = list()
-                for statkey_pair in statkey_yaml[collector]:
+
+                def append_metrics(statkey_pair):
                     multiple_metrics_generated.append({"resourceId": "3628-93a1-56e84634050814", "stat-list": {"stat": [
                         {"timestamps": [1582797716394], "statKey": {"key": statkey_pair['statkey']}, "data": [88.0]}]}})
                     multiple_metrics_generated.append({"resourceId": "5628-9ba1-55e847050815", "stat-list": {"stat": [
                         {"timestamps": [1582797716394], "statKey": {"key": statkey_pair['statkey']}, "data": [44.0]}]}})
                     multiple_metrics_generated.append({"resourceId": "7422-91h7-52s842060815", "stat-list": {"stat": [
                         {"timestamps": [1582797716394], "statKey": {"key": statkey_pair['statkey']}, "data": [55.0]}]}})
+
+                if collector == 'VMStatsCollector':
+                    for statkey_pair in statkey_yaml[collector][rubric]:
+                        append_metrics(statkey_pair)
+                else:
+                    for statkey_pair in statkey_yaml[collector]:
+                        append_metrics(statkey_pair)
                 Vrops.get_latest_stat_multiple = MagicMock(return_value=multiple_metrics_generated)
 
             if "Properties" in collector:
@@ -142,15 +148,15 @@ class TestCollectors(unittest.TestCase):
 
             # start prometheus server to provide metrics later on
             collector_instance = globals()[collector]()
-            thread1 = Thread(target=run_prometheus_server, args=(random_prometheus_port, [collector_instance]))
+            thread1 = Thread(target=run_prometheus_server, args=(self.random_prometheus_port, [collector_instance]))
             thread1.daemon = True
             thread1.start()
             thread_list.append(thread1)
             # give grandpa thread some time to get prometheus started and run a couple intervals of InventoryBuilder
             time.sleep(10)
 
-            print("prometheus query port " + str(random_prometheus_port))
-            c = http.client.HTTPConnection("localhost:" + str(random_prometheus_port))
+            print("prometheus query port " + str(self.random_prometheus_port))
+            c = http.client.HTTPConnection("localhost:" + str(self.random_prometheus_port))
             c.request("GET", "/")
             r = c.getresponse()
             self.assertEqual(r.status, 200, "HTTP server return code should be 200")
@@ -173,7 +179,10 @@ class TestCollectors(unittest.TestCase):
                     continue
                 metrics.add(split_entry[0] + "}")
 
-            metrics_yaml_list = metrics_yaml[collector]['metrics']
+            if collector == 'VMStatsCollector':
+                metrics_yaml_list = metrics_yaml[collector]['metrics'][rubric]
+            else:
+                metrics_yaml_list = metrics_yaml[collector]['metrics']
             self.assertTrue(metrics_yaml_list, msg=collector + " has no metrics defined, FIX IT!")
             self.assertTrue(metrics, msg=collector + " is not producing any metrics at all, how should I continue?")
 
@@ -191,8 +200,18 @@ class TestCollectors(unittest.TestCase):
                 t.join(timeout=5)
 
             # we don't want to have any port locks if prometheus server thread is not shutting down
-            random_prometheus_port += 1
+            self.random_prometheus_port += 1
             REGISTRY.unregister(collector_instance)
+
+        for collector in metrics_yaml.keys():
+            print("\nTesting " + collector)
+            if collector == 'VMStatsCollector':
+                for rubric in metrics_yaml[collector]['metrics']:
+                    os.environ['RUBRIC'] = rubric
+                    print("Testing rubric:", rubric)
+                    collector_testrun(rubric)
+            else:
+                collector_testrun()
 
 
 if __name__ == '__main__':
