fa35f670753cc9b3ac57dae2c99afa9b66ca84fa
Richard Tief <richard.tief@sap.com>
2020-11-27 16:13:39 +0100

    change path to collector config

--- tests/TestLaunchExporter.py
+++ tests/TestLaunchExporter.py
@@ -16,28 +16,28 @@ class TestLaunchExporter(TestCase):
     # test with debug option on
     def test_with_cli_params_1(self):
 
-        sys.argv = ['prog', '-o', '1234', '-i', 'inventory.some.url', '-m', 'collector_config.yaml', '-t',
+        sys.argv = ['prog', '-o', '1234', '-i', 'inventory.some.url', '-m', 'tests/collector_config.yaml', '-t',
                     'testhost.test']
         parse_params(logger=logger)
         self.assertEqual(os.getenv('PORT'), '1234', 'The port was not set correctly!')
         self.assertEqual(os.getenv('INVENTORY'), 'inventory.some.url', 'Inventory was not set correctly')
-        self.assertEqual(os.getenv('CONFIG'), 'collector_config.yaml', 'Config was not set')
+        self.assertEqual(os.getenv('CONFIG'), 'tests/collector_config.yaml', 'Config was not set')
         self.assertEqual(os.getenv('TARGET'), 'testhost.test', 'Target was not set')
 
     # test with debug option off
     def test_with_cli_params_2(self):
         os.environ.clear()
-        sys.argv = ['prog', '--port', '1234', '-i', 'inventory.some.url', '-m', 'collector_config.yaml', '-t',
+        sys.argv = ['prog', '--port', '1234', '-i', 'inventory.some.url', '-m', 'tests/collector_config.yaml', '-t',
                     'testhost.test']
         parse_params(logger)
         self.assertEqual(os.getenv('PORT'), '1234', 'The port was not set correctly!')
         self.assertEqual(os.getenv('INVENTORY'), 'inventory.some.url', 'Inventory was not set correctly')
-        self.assertEqual(os.getenv('CONFIG'), 'collector_config.yaml', 'Config was not set')
+        self.assertEqual(os.getenv('CONFIG'), 'tests/collector_config.yaml', 'Config was not set')
         self.assertEqual(os.getenv('TARGET'), 'testhost.test', 'Target was not set')
 
     def test_with_cli_and_env_params(self):
         sys.argv = ['prog', '--port', '1234', '-i', 'inventory.some.url', '-m',
-                    'collector_config.yaml', '-t', 'testhost.test']
+                    'tests/collector_config.yaml', '-t', 'testhost.test']
         os.environ['PORT'] = '1123'
         os.environ['INVENTORY'] = 'inventory.wedontwantthis.url'
         os.environ['CONFIG'] = 'tests/other_config.yaml'
@@ -46,20 +46,20 @@ class TestLaunchExporter(TestCase):
         # cli params preferred
         self.assertEqual(os.getenv('PORT'), '1234', 'The port was not set correctly!')
         self.assertEqual(os.getenv('INVENTORY'), 'inventory.some.url', 'Inventory was not set correctly')
-        self.assertEqual(os.getenv('CONFIG'), 'collector_config.yaml', 'Config was not set')
+        self.assertEqual(os.getenv('CONFIG'), 'tests/collector_config.yaml', 'Config was not set')
         self.assertEqual(os.getenv('TARGET'), 'testhost.test', 'Target was not set')
 
     # test use default collectors when nothing is specified
     def test_with_no_collector(self):
         sys.argv = ['prog', '--port', '1234', '-i', 'inventory.some.url', '-m',
-                    'collector_config.yaml', '-t', 'testhost.test']
+                    'tests/collector_config.yaml', '-t', 'testhost.test']
         options = parse_params(logger)
         self.assertEqual(options.collectors, default_collectors(), 'Default collector list does not match the default')
 
     # test with only one collector enabled
     def test_with_one_collector(self):
         sys.argv = ['prog', '--port', '1234', '-i', 'inventory.some.url', '-c', 'VMStatsCollector', '-m',
-                    'collector_config.yaml', '-t', 'testhost.test']
+                    'tests/collector_config.yaml', '-t', 'testhost.test']
         options = parse_params(logger)
         self.assertEqual(options.collectors, ['VMStatsCollector'],
                          'Collector list does not match given single collector')
@@ -67,7 +67,7 @@ class TestLaunchExporter(TestCase):
     # test multiple enabled collectors
     def test_with_multiple_collector(self):
         sys.argv = ['prog', '--port', '1234', '-i', 'inventory.some.url', '-c', 'VMStatsCollector', '-m',
-                    'collector_config.yaml', '-c', 'VMPropertiesCollector', '-t', 'testhost.test']
+                    'tests/collector_config.yaml', '-c', 'VMPropertiesCollector', '-t', 'testhost.test']
         options = parse_params(logger)
         self.assertEqual(options.collectors, ['VMStatsCollector', 'VMPropertiesCollector'],
                          'Collector list does not match given multiple collectors')
