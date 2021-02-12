c8ced0409bddf32b1b392d4270fe939b197c4d2d
Tommy Sauer <tommy.sauer@sap.com>
2021-02-12 13:58:08 +0100

    parallel calls for vrops targets (#140)
    threading introduced for multiple runs to improve runtime.
    shared timeout for threading
    logging how long each target took to fetch its data

--- tests/TestLaunchInventory.py
+++ tests/TestLaunchInventory.py
@@ -20,7 +20,7 @@ class TestLaunchExporter(TestCase):
     # test with debug option on
     def test_with_cli_params_1(self):
         sys.argv = ['prog', '-u', 'testuser', '-p', 'testpw31!', '-o', '1234',
-                    '-a', '/path/to/atlas.yaml', '-l', '-s', '180', '-d']
+                    '-a', '/path/to/atlas.yaml', '-l', '-s', '180', '-t', '300', '-d']
         parse_params(logger)
         self.assertEqual(os.getenv('USER'), 'testuser', 'The user was not set correctly!')
         self.assertEqual(os.getenv('PASSWORD'), 'testpw31!', 'The password was not set correctly!')
@@ -28,13 +28,14 @@ class TestLaunchExporter(TestCase):
         self.assertEqual(os.getenv('ATLAS'), '/path/to/atlas.yaml', 'Atlas was not set correctly')
         self.assertEqual(os.getenv('LOOPBACK'), '1', 'Loopback was not set correctly')
         self.assertEqual(os.getenv('SLEEP'), '180', 'Sleep time was not set correctly')
+        self.assertEqual(os.getenv('TIMEOUT'), '300', 'Timeout time was not set correctly')
         self.assertEqual(logger.level, 10)
 
     # test with debug option off
     def test_with_cli_params_2(self):
         os.environ.clear()
         sys.argv = ['prog', '--user', 'testuser', '--password', 'testpw31!', '--port', '1234',
-                    '-a', '/path/to/atlas.yaml', '-l', '--sleep', '180', '--v']
+                    '-a', '/path/to/atlas.yaml', '-l', '--sleep', '180', '-t', '300', '--v']
         parse_params(logger)
         self.assertEqual(os.getenv('USER'), 'testuser', 'The user was not set correctly!')
         self.assertEqual(os.getenv('PASSWORD'), 'testpw31!', 'The password was not set correctly!')
@@ -42,17 +43,19 @@ class TestLaunchExporter(TestCase):
         self.assertEqual(os.getenv('ATLAS'), '/path/to/atlas.yaml', 'Atlas was not set correctly')
         self.assertEqual(os.getenv('LOOPBACK'), '1', 'Loopback was not set correctly')
         self.assertEqual(os.getenv('SLEEP'), '180', 'Sleep time was not set correctly')
+        self.assertEqual(os.getenv('TIMEOUT'), '300', 'Timeout time was not set correctly')
         self.assertEqual(logger.level, 20)
 
     def test_with_cli_and_env_params(self):
         sys.argv = ['prog', '--user', 'cli_testuser', '--password', 'testpw31!',
-                    '--port', '1234', '-a', '/path/to/atlas.yaml', '-l', '-s', '180']
+                    '--port', '1234', '-a', '/path/to/atlas.yaml', '-l', '-s', '180', '-t', '300']
         os.environ['USER'] = 'env_testuser'
         os.environ['PASSWORD'] = 'testps31!_2'
         os.environ['PORT'] = '1123'
         os.environ['ATLAS'] = '/wrong/path/to/atlas.yaml'
         os.environ['LOOPBACK'] = '0'
         os.environ['SLEEP'] = '199'
+        os.environ['TIMEOUT'] = '301'
         parse_params(logger)
         # cli params preferred
         self.assertEqual(os.getenv('USER'), 'cli_testuser', 'The user was not set correctly!')
@@ -61,6 +64,7 @@ class TestLaunchExporter(TestCase):
         self.assertEqual(os.getenv('ATLAS'), '/path/to/atlas.yaml', 'Atlas was not set correctly')
         self.assertEqual(os.getenv('LOOPBACK'), '1', 'Loopback was not set correctly')
         self.assertEqual(os.getenv('SLEEP'), '180', 'Sleep time was not set correctly')
+        self.assertEqual(os.getenv('TIMEOUT'), '300', 'Timeout time was not set correctly')
         self.assertEqual(logger.level, 30)
 
     def test_with_bogus_options(self):
