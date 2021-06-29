3044152e93907ec010ecd4fc5c3517dccf98f2e7
Richard Tief <richard.tief@sap.com>
2021-06-29 14:46:43 +0200

    adjusted testcase

--- collectors/StatsCollector.py
+++ collectors/StatsCollector.py
@@ -53,7 +53,7 @@ class StatsCollector(BaseCollector):
             for value_entry in resource.get('stat-list', {}).get('stat', []):
                 statkey = value_entry.get('statKey', {}).get('key')
                 # Normalisation of keys retrieved from API (e.g. cpu:102|usage_average -> cpu|usage_average)
-                norm_statkey = re.sub("[^a-zA-Z|_\- ]+", "", statkey)
+                norm_statkey = re.sub("[^a-zA-Z|_ -]+", "", statkey)
                 values_received.add(norm_statkey)
 
                 metric_data = value_entry.get('data', [0])[0]
