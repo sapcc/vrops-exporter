b05ca450768680d0ef046378314bb4705e6535e0
Tommy Sauer <tommy.sauer@sap.com>
2021-08-24 10:39:27 +0200

    fixing memory leak Using vrops object here results in adding a new list with the object hash reference for every run. Using the short name instead will provide the wantet overwrite feature

--- InventoryBuilder.py
+++ InventoryBuilder.py
@@ -221,8 +221,8 @@ class InventoryBuilder:
         vcenter = self.create_vcenter_objects(vrops, target, token)
         nsxt_adapter = self.create_nsxt_objects(vrops, target, token)
 
-        self.vcenter_dict[vrops] = vcenter
-        self.nsxt_dict[vrops] = nsxt_adapter
+        self.vcenter_dict[vrops_short_name] = vcenter
+        self.nsxt_dict[vrops_short_name] = nsxt_adapter
 
         if iteration == 1:
             self.alertdefinitions = Vrops.get_alertdefinitions(vrops, target, token)
