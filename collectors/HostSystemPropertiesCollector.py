23380cfa6de0d71c1e11f03f2ac23ca4153c7023
Tommy Sauer <tommy.sauer@sap.com>
2021-09-01 12:15:52 +0200

    exposing internal_name label for hosts

--- collectors/HostSystemPropertiesCollector.py
+++ collectors/HostSystemPropertiesCollector.py
@@ -6,7 +6,7 @@ class HostSystemPropertiesCollector(PropertiesCollector):
     def __init__(self):
         super().__init__()
         self.vrops_entity_name = 'hostsystem'
-        self.label_names = [self.vrops_entity_name, 'vcenter', 'datacenter', 'vccluster']
+        self.label_names = [self.vrops_entity_name, 'vcenter', 'datacenter', 'vccluster', 'internal_name']
 
     def get_resource_uuids(self):
         return self.get_hosts_by_target()
@@ -15,4 +15,5 @@ class HostSystemPropertiesCollector(PropertiesCollector):
         return [self.hosts[resource_id]['name'],
                 self.hosts[resource_id]['vcenter'],
                 self.hosts[resource_id]['datacenter'].lower(),
-                self.hosts[resource_id]['parent_cluster_name']] if resource_id in self.hosts else []
+                self.hosts[resource_id]['parent_cluster_name'],
+                self.hosts[resource_id]['internal_name']] if resource_id in self.hosts else []
--- collectors/HostSystemStatsCollector.py
+++ collectors/HostSystemStatsCollector.py
@@ -5,7 +5,7 @@ class HostSystemStatsCollector(StatsCollector):
     def __init__(self):
         super().__init__()
         self.vrops_entity_name = 'hostsystem'
-        self.label_names = [self.vrops_entity_name, 'vcenter', 'datacenter', 'vccluster']
+        self.label_names = [self.vrops_entity_name, 'vcenter', 'datacenter', 'vccluster', 'internal_name']
 
     def get_resource_uuids(self):
         return self.get_hosts_by_target()
@@ -14,4 +14,5 @@ class HostSystemStatsCollector(StatsCollector):
         return [self.hosts[resource_id]['name'],
                 self.hosts[resource_id]['vcenter'],
                 self.hosts[resource_id]['datacenter'].lower(),
-                self.hosts[resource_id]['parent_cluster_name']] if resource_id in self.hosts else []
+                self.hosts[resource_id]['parent_cluster_name'],
+                self.hosts[resource_id]['internal_name']] if resource_id in self.hosts else []
