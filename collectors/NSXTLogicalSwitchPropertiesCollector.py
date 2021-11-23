1181661ee81d52a572d3e7899f28d6132ce7bbb1
Richard Tief <mail@richardtief.de>
2021-11-23 13:57:24 +0100

    NSXTLogicalSwitchPropertiesCollector added

---
+++ collectors/NSXTLogicalSwitchPropertiesCollector.py
@@ -0,0 +1,17 @@
+from collectors.PropertiesCollector import PropertiesCollector
+
+
+class NSXTLogicalSwitchPropertiesCollector(PropertiesCollector):
+
+    def __init__(self):
+        super().__init__()
+        self.vrops_entity_name = 'nsxt_logical_switch'
+        self.label_names = ['nsxt_mgmt_cluster', 'nsxt_adapter', self.vrops_entity_name]
+
+    def get_resource_uuids(self):
+        return self.get_nsxt_logical_switches_by_target()
+
+    def get_labels(self, resource_id, project_ids):
+        return [self.nsxt_logical_switches[resource_id]['mgmt_cluster_name'],
+                self.nsxt_logical_switches[resource_id]['nsxt_adapter_name'],
+                self.nsxt_logical_switches[resource_id]['name']] if resource_id in self.nsxt_logical_switches else []
