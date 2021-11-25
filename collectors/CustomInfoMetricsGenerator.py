5188cef2e0f456472a6d82ea58ab83d2f3c858d2
Richard Tief <mail@richardtief.de>
2021-11-25 14:02:12 +0100

    CustomInfoMetricsGenerator added

---
+++ collectors/CustomInfoMetricsGenerator.py
@@ -0,0 +1,26 @@
+from BaseCollector import BaseCollector
+
+from prometheus_client.core import InfoMetricFamily
+import logging
+
+logger = logging.getLogger('vrops-exporter')
+
+
+class CustomInfoMetricsGenerator(BaseCollector):
+
+    def __init__(self):
+        super().__init__()
+        self.name = self.__class__.__name__
+        self.custom_metrics = self.read_collector_config().get('CustomInfoMetricsGenerator')
+
+    def describe(self):
+        for entry in self.custom_metrics:
+            yield InfoMetricFamily(entry['metric'], 'vrops-exporter')
+
+    def collect(self):
+        logger.info(f'{self.name} starts with collecting the metrics')
+
+        for entry in self.custom_metrics:
+            custom_info_metric = InfoMetricFamily(entry['metric'], 'vrops-exporter', labels=['target'])
+            custom_info_metric.add_metric(labels=[self.target], value=entry['values_dict'])
+            yield custom_info_metric
