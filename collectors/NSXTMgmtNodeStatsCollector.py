5d01b74290ff8b458b0b5056f8d4c9e1269971dc
Richard Tief <mail@richardtief.de>
2022-02-18 11:03:10 +0100

    target label added

--- collectors/NSXTMgmtNodeStatsCollector.py
+++ collectors/NSXTMgmtNodeStatsCollector.py
@@ -6,7 +6,7 @@ class NSXTMgmtNodeStatsCollector(StatsCollector):
     def __init__(self):
         super().__init__()
         self.vrops_entity_name = 'nsxt_mgmt_node'
-        self.label_names = ['nsxt_mgmt_cluster', 'nsxt_adapter', 'nsxt_mgmt_node']
+        self.label_names = ['nsxt_mgmt_cluster', 'nsxt_adapter', 'nsxt_mgmt_node', 'target']
 
     def get_resource_uuids(self):
         return self.get_nsxt_mgmt_nodes_by_target()
@@ -14,4 +14,5 @@ class NSXTMgmtNodeStatsCollector(StatsCollector):
     def get_labels(self, resource_id, project_ids):
         return [self.nsxt_mgmt_nodes[resource_id]['mgmt_cluster_name'],
                 self.nsxt_mgmt_nodes[resource_id]['nsxt_adapter_name'],
-                self.nsxt_mgmt_nodes[resource_id]['name']] if resource_id in self.nsxt_mgmt_nodes else []
+                self.nsxt_mgmt_nodes[resource_id]['name'],
+                self.nsxt_mgmt_nodes[resource_id]['target']] if resource_id in self.nsxt_mgmt_nodes else []
