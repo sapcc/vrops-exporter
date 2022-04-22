1eaca2954d7884e7ba888acc9314a51169084381
d066607 <tommy.sauer@sap.com>
2022-04-22 11:54:19 +0200

    testhost changed changing testhost to match documentation for consistency

--- tests/TestLaunchExporter.py
+++ tests/TestLaunchExporter.py
@@ -24,52 +24,52 @@ class TestLaunchExporter(TestCase):
     # test with debug option on
     def test_with_cli_params_1(self):
         sys.argv = ['prog', '-o', '1234', '-i', 'inventory.some.url', '-m', 'tests/collector_config.yaml', '-t',
-                    'testhost.test', '-d']
+                    'vrops-vcenter-test.company.com', '-d']
         parse_params(logger)
         self.assertEqual(os.getenv('PORT'), '1234', 'The port was not set correctly!')
         self.assertEqual(os.getenv('INVENTORY'), 'inventory.some.url', 'Inventory was not set correctly')
         self.assertEqual(os.getenv('COLLECTOR_CONFIG'), 'tests/collector_config.yaml', 'Config was not set')
-        self.assertEqual(os.getenv('TARGET'), 'testhost.test', 'Target was not set')
+        self.assertEqual(os.getenv('TARGET'), 'vrops-vcenter-test.company.com', 'Target was not set')
         self.assertEqual(logger.level, 10)
 
     # test with debug option off
     def test_with_cli_params_2(self):
         os.environ.clear()
         sys.argv = ['prog', '--port', '1234', '-i', 'inventory.some.url', '-m', 'tests/collector_config.yaml', '-t',
-                    'testhost.test']
+                    'vrops-vcenter-test.company.com']
         parse_params(logger)
         self.assertEqual(os.getenv('PORT'), '1234', 'The port was not set correctly!')
         self.assertEqual(os.getenv('INVENTORY'), 'inventory.some.url', 'Inventory was not set correctly')
         self.assertEqual(os.getenv('COLLECTOR_CONFIG'), 'tests/collector_config.yaml', 'Config was not set')
-        self.assertEqual(os.getenv('TARGET'), 'testhost.test', 'Target was not set')
+        self.assertEqual(os.getenv('TARGET'), 'vrops-vcenter-test.company.com', 'Target was not set')
         self.assertEqual(logger.level, 30)
 
     def test_with_cli_and_env_params(self):
         sys.argv = ['prog', '--port', '1234', '-i', 'inventory.some.url', '-m',
-                    'tests/collector_config.yaml', '-t', 'testhost.test', '-v']
+                    'tests/collector_config.yaml', '-t', 'vrops-vcenter-test.company.com', '-v']
         os.environ['PORT'] = '1123'
         os.environ['INVENTORY'] = 'inventory.wedontwantthis.url'
         os.environ['COLLECTOR_CONFIG'] = 'tests/other_config.yaml'
-        os.environ['TARGET'] = 'Othertesthost.test'
+        os.environ['TARGET'] = 'Othervrops-vcenter-test.company.com'
         parse_params(logger)
         # cli params preferred
         self.assertEqual(os.getenv('PORT'), '1234', 'The port was not set correctly!')
         self.assertEqual(os.getenv('INVENTORY'), 'inventory.some.url', 'Inventory was not set correctly')
         self.assertEqual(os.getenv('COLLECTOR_CONFIG'), 'tests/collector_config.yaml', 'Config was not set')
-        self.assertEqual(os.getenv('TARGET'), 'testhost.test', 'Target was not set')
+        self.assertEqual(os.getenv('TARGET'), 'vrops-vcenter-test.company.com', 'Target was not set')
         self.assertEqual(logger.level, 20)
 
     # test use default collectors when nothing is specified
     def test_with_no_collector(self):
         sys.argv = ['prog', '--port', '1234', '-i', 'inventory.some.url', '-m',
-                    'tests/collector_config.yaml', '-t', 'testhost.test']
+                    'tests/collector_config.yaml', '-t', 'vrops-vcenter-test.company.com']
         options = parse_params(logger)
         self.assertEqual(options.collectors, default_collectors(), 'Default collector list does not match the default')
 
     # test with only one collector enabled
     def test_with_one_collector(self):
         sys.argv = ['prog', '--port', '1234', '-i', 'inventory.some.url', '-c', 'VMStatsCollector', '-m',
-                    'tests/collector_config.yaml', '-t', 'testhost.test']
+                    'tests/collector_config.yaml', '-t', 'vrops-vcenter-test.company.com']
         options = parse_params(logger)
         self.assertEqual(options.collectors, ['VMStatsCollector'],
                          'Collector list does not match given single collector')
@@ -77,7 +77,7 @@ class TestLaunchExporter(TestCase):
     # test multiple enabled collectors
     def test_with_multiple_collector(self):
         sys.argv = ['prog', '--port', '1234', '-i', 'inventory.some.url', '-c', 'VMStatsCollector', '-m',
-                    'tests/collector_config.yaml', '-c', 'VMPropertiesCollector', '-t', 'testhost.test']
+                    'tests/collector_config.yaml', '-c', 'VMPropertiesCollector', '-t', 'vrops-vcenter-test.company.com']
         options = parse_params(logger)
         self.assertEqual(options.collectors, ['VMStatsCollector', 'VMPropertiesCollector'],
                          'Collector list does not match given multiple collectors')
