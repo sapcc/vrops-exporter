06b1ef0b39afa1a8a3dbf8ac19db738c910d3a0b
Richard Tief <mail@richardtief.de>
2022-11-18 15:34:55 +0100

    fix storagepod labeling

--- collectors/DatastorePropertiesCollector.py
+++ collectors/DatastorePropertiesCollector.py
@@ -7,18 +7,23 @@ class DatastorePropertiesCollector(PropertiesCollector):
         super().__init__()
         self.vrops_entity_name = 'datastore'
         self.label_names = [self.vrops_entity_name,
-                            'vcenter', 'type', 'datacenter', 'storagepod']
+                            'vcenter', 'type', 'datacenter']
 
     def get_resource_uuids(self):
         return self.get_datastores_by_target()
 
     def get_labels(self, resource_id, project_ids):
+        if 'storagepod' in self.label_names:
+            self.label_names.pop()
+
         label_values = [self.datastores[resource_id]['name'],
                         self.datastores[resource_id]['vcenter'],
                         self.datastores[resource_id]['type'],
                         self.datastores[resource_id]['parent_dc_name'].lower()] if resource_id in self.datastores else []
 
         if sc_name := self.datastores[resource_id].get('storage_cluster_name'):
+            self.label_names.append('storagepod')
             label_values.append(sc_name)
+
 
         return label_values
