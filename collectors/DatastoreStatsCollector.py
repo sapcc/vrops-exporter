9b26bd90bf4959c2c5924bbe2986f1322e4106f9
Richard Tief <mail@richardtief.de>
2022-11-21 08:37:03 +0100

    add storagepod default label

--- collectors/DatastoreStatsCollector.py
+++ collectors/DatastoreStatsCollector.py
@@ -12,16 +12,14 @@ class DatastoreStatsCollector(StatsCollector):
         return self.get_datastores_by_target()
 
     def get_labels(self, resource_id, project_ids):
-        if 'storagepod' in self.label_names:
-            self.label_names.pop()
-
         label_values = [self.datastores[resource_id]['name'],
                         self.datastores[resource_id]['vcenter'],
                         self.datastores[resource_id]['type'],
                         self.datastores[resource_id]['parent_dc_name'].lower()] if resource_id in self.datastores else []
 
         if sc_name := self.datastores[resource_id].get('storage_cluster_name'):
-            self.label_names.append('storagepod')
             label_values.append(sc_name)
+        else:
+            label_values.append("none")
 
         return label_values
