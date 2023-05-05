b0063045552275e949058aeaf984ac535d02d92e
Richard Tief <mail@richardtief.de>
2023-05-05 15:55:02 +0200

    fix CustomInfoMetricsCollector

--- collectors/CustomInfoMetricsGenerator.py
+++ collectors/CustomInfoMetricsGenerator.py
@@ -11,7 +11,6 @@ class CustomInfoMetricsGenerator(BaseCollector):
     def __init__(self):
         super().__init__()
         self.name = self.__class__.__name__
-        self.custom_metrics = self.read_collector_config().get('CustomInfoMetricsGenerator')
 
     def describe(self):
         for entry in self.custom_metrics:
@@ -20,7 +19,8 @@ class CustomInfoMetricsGenerator(BaseCollector):
     def collect(self):
         logger.info(f'{self.name} starts with collecting the metrics')
 
-        for entry in self.custom_metrics:
+        custom_metrics = self.read_collector_config().get('CustomInfoMetricsGenerator')
+        for entry in custom_metrics:
             custom_info_metric = InfoMetricFamily(entry['metric'], 'vrops-exporter', labels=['target'])
             custom_info_metric.add_metric(labels=[self.target], value=entry['values_dict'])
             yield custom_info_metric
